44badcce77fec63a392191b4aa87d3548d6d5a04
Tai Sakuma <tai.sakuma@gmail.com>
2025-10-30 07:58:40 -0400

    Move code to `util.py`

--- src/hypothesis_awkward/util.py
+++ src/hypothesis_awkward/util.py
@@ -2,6 +2,42 @@ from collections.abc import Mapping
 
 import numpy as np
 
+from awkward.types.numpytype import _primitive_to_dtype_dict, primitive_to_dtype
+
+
+def _supported_dtype_names() -> tuple[str, ...]:
+    '''Return names of NumPy scalar dtypes supported by Awkward Array.
+
+    I.e., ('int32', 'float64', 'datetime64[ns]', ...)
+    '''
+    DATETIME_UNITS = tuple('Y M W D h m s ms us ns ps fs as'.split())
+
+    # ('bool', 'int8', ...)
+    base = tuple(
+        n
+        for n, d in _primitive_to_dtype_dict.items()
+        if d.kind not in ('M', 'm')  # Exclude datetime/timedelta as they need units
+    )
+
+    # ('datetime64[Y]', 'datetime64[M]', ...)
+    dt = tuple(f'datetime64[{unit}]' for unit in DATETIME_UNITS)
+
+    # ('timedelta64[Y]', 'timedelta64[M]', ...)
+    td = tuple(f'timedelta64[{unit}]' for unit in DATETIME_UNITS)
+
+    return base + dt + td
+
+
+# Names of NumPy dtypes supported by Awkward Array
+# ('bool', 'int8', 'float16', 'datetime64[ns]', ...)
+SUPPORTED_DTYPE_NAMES = _supported_dtype_names()
+
+# NumPy dtypes supported by Awkward Array
+# (dtype('bool'), dtype('int8'), dtype('float16'), dtype('datetime64[ns]'), ...)
+SUPPORTED_DTYPES = tuple[np.dtype, ...](
+    primitive_to_dtype(name) for name in SUPPORTED_DTYPE_NAMES
+)
+
 
 def simple_dtypes_in(d: np.dtype) -> set[np.dtype]:
     '''Simple dtypes contained in `d`.'''
