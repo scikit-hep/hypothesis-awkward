2ed9aab1a5c9224fbc8f07fb7b7334e1a6e6b5c1
Tai Sakuma <tai.sakuma@gmail.com>
2025-10-30 07:15:55 -0400

    Extract `simple_dtypes_in()`

--- src/hypothesis_awkward/util.py
+++ src/hypothesis_awkward/util.py
@@ -3,20 +3,25 @@ from collections.abc import Mapping
 import numpy as np
 
 
-def simple_dtype_kinds_in(d: np.dtype) -> set[str]:
-    '''Kinds of simple dtypes (e.g. `i`, `f`, `M`) contained in `d`.'''
+def simple_dtypes_in(d: np.dtype) -> set[np.dtype]:
+    '''Simple dtypes contained in `d`.'''
     match d.names, d.kind, d.subdtype, d.fields:
-        case None, kind, None, None:
+        case None, str(), None, None:
             # Simple dtype
             # e.g., d = dtype('int32')
-            return {kind}
+            return {d}
         case None, 'V', tuple(subdtype), None:
             # Sub-array dtype
             # e.g., d = dtype(('int32', (3, 4)))
-            return {subdtype[0].kind}
+            return simple_dtypes_in(subdtype[0])
         case tuple(names), 'V', None, fields if isinstance(fields, Mapping):
             # Structured dtype
             # e.g., d = dtype([('f0', 'i4'), ('f1', 'f8')])
-            return {k for n in names for k in simple_dtype_kinds_in(fields[n][0])}
+            return {t for n in names for t in simple_dtypes_in(fields[n][0])}
         case _:
             raise TypeError(f'Unexpected dtype: {d}')
+
+
+def simple_dtype_kinds_in(d: np.dtype) -> set[str]:
+    '''Kinds of simple dtypes (e.g. `i`, `f`, `M`) contained in `d`.'''
+    return {t.kind for t in simple_dtypes_in(d)}
